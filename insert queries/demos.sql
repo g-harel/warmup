39e24c5040b5cd39ca27538e6b75c42516b22110
Gabriel Harel <gabrielj.harel@gmail.com>
2018-02-11 17:45:16 -0500

    formatted insert queries

--- insert queries/demos.sql
+++ insert queries/demos.sql
@@ -1,28 +1,32 @@
-CREATE TABLE demos (tid INT NOT NULL, sid INT NOT NULL, `datetime` DATETIME, grade CHAR(1));
+CREATE TABLE demos (
+    tid      INT      NOT NULL,
+    sid      INT      NOT NULL,
+    datetime DATETIME,
+    grade    CHAR(1)
+);
 
-insert into demos (tid, sid, datetime, grade) values (709, 467, '2018-01-09 01:54:11', 'a');
-insert into demos (tid, sid, datetime, grade) values (850, 988, '2018-01-17 14:30:20', 'b');
-insert into demos (tid, sid, datetime, grade) values (498, 932, '2018-02-14 09:24:28', 'c');
-insert into demos (tid, sid, datetime, grade) values (134, 703, '2018-02-06 07:32:56', 'd');
-insert into demos (tid, sid, datetime, grade) values (138, 183, '2018-01-18 00:34:10', 'f');
-insert into demos (tid, sid, datetime, grade) values (842, 421, '2018-02-06 00:03:13', 'a');
-insert into demos (tid, sid, datetime, grade) values (826, 250, '2018-01-03 05:42:10', 'b');
-insert into demos (tid, sid, datetime, grade) values (529, 152, '2018-01-16 15:35:42', 'c');
-insert into demos (tid, sid, datetime, grade) values (135, 750, '2018-02-25 02:48:14', 'd');
-insert into demos (tid, sid, datetime, grade) values (650, 978, '2018-02-18 12:52:05', 'f');
-insert into demos (tid, sid, datetime, grade) values (248, 684, '2018-02-21 03:50:32', 'a');
-insert into demos (tid, sid, datetime, grade) values (414, 615, '2018-01-28 04:21:55', 'b');
-insert into demos (tid, sid, datetime, grade) values (223, 264, '2018-01-18 13:54:59', 'c');
-insert into demos (tid, sid, datetime, grade) values (751, 859, '2018-02-14 14:29:27', 'd');
-insert into demos (tid, sid, datetime, grade) values (338, 494, '2018-01-19 20:29:16', 'f');
-insert into demos (tid, sid, datetime, grade) values (310, 907, '2018-02-09 02:59:28', 'a');
-insert into demos (tid, sid, datetime, grade) values (515, 101, '2018-02-14 02:18:39', 'b');
-insert into demos (tid, sid, datetime, grade) values (210, 736, '2018-01-18 16:10:21', 'c');
-insert into demos (tid, sid, datetime, grade) values (213, 483, '2018-02-12 21:42:23', 'd');
-insert into demos (tid, sid, datetime, grade) values (629, 446, '2018-01-20 17:05:58', 'a');
-insert into demos (tid, sid, datetime, grade) values (239, 681, '2018-02-15 07:51:07', 'b');
-insert into demos (tid, sid, datetime, grade) values (716, 381, '2018-02-13 11:15:15', 'c');
-insert into demos (tid, sid, datetime, grade) values (878, 165, '2018-01-20 02:26:53', 'd');
-insert into demos (tid, sid, datetime, grade) values (203, 525, '2018-01-18 17:04:55', 'f');
-insert into demos (tid, sid, datetime, grade) values (641, 553, '2018-02-02 04:10:52', 'a');
-
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (709, 467, '2018-01-09 01:54:11', 'a');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (850, 988, '2018-01-17 14:30:20', 'b');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (498, 932, '2018-02-14 09:24:28', 'c');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (134, 703, '2018-02-06 07:32:56', 'd');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (138, 183, '2018-01-18 00:34:10', 'f');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (842, 421, '2018-02-06 00:03:13', 'a');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (826, 250, '2018-01-03 05:42:10', 'b');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (529, 152, '2018-01-16 15:35:42', 'c');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (135, 750, '2018-02-25 02:48:14', 'd');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (650, 978, '2018-02-18 12:52:05', 'f');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (248, 684, '2018-02-21 03:50:32', 'a');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (414, 615, '2018-01-28 04:21:55', 'b');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (223, 264, '2018-01-18 13:54:59', 'c');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (751, 859, '2018-02-14 14:29:27', 'd');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (338, 494, '2018-01-19 20:29:16', 'f');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (310, 907, '2018-02-09 02:59:28', 'a');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (515, 101, '2018-02-14 02:18:39', 'b');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (210, 736, '2018-01-18 16:10:21', 'c');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (213, 483, '2018-02-12 21:42:23', 'd');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (629, 446, '2018-01-20 17:05:58', 'a');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (239, 681, '2018-02-15 07:51:07', 'b');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (716, 381, '2018-02-13 11:15:15', 'c');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (878, 165, '2018-01-20 02:26:53', 'd');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (203, 525, '2018-01-18 17:04:55', 'f');
+INSERT INTO demos (tid, sid, datetime, grade) VALUES (641, 553, '2018-02-02 04:10:52', 'a');
--- insert queries/teams.sql
+++ insert queries/teams.sql
@@ -1,23 +1,26 @@
-CREATE TABLE teams (leaderId INT NOT NULL, noOfMembers TINYINT NOT NULL, tid INT PRIMARY KEY);
+CREATE TABLE teams (
+    tid         INT     PRIMARY KEY
+    leaderId    INT     NOT NULL,
+    noOfMembers TINYINT NOT NULL,
+);
 
-insert into teams (leaderId, noOfMembers, TID) values (346, 4, 467);
-insert into teams (leaderId, noOfMembers, TID) values (492, 3, 988);
-insert into teams (leaderId, noOfMembers, TID) values (629, 3, 932);
-insert into teams (leaderId, noOfMembers, TID) values (223, 3, 703);
-insert into teams (leaderId, noOfMembers, TID) values (661, 4, 183);
-insert into teams (leaderId, noOfMembers, TID) values (532, 2, 421);
-insert into teams (leaderId, noOfMembers, TID) values (325, 4, 250);
-insert into teams (leaderId, noOfMembers, TID) values (727, 4, 152);
-insert into teams (leaderId, noOfMembers, TID) values (349, 4, 750);
-insert into teams (leaderId, noOfMembers, TID) values (570, 2, 978);
-insert into teams (leaderId, noOfMembers, TID) values (708, 3, 684);
-insert into teams (leaderId, noOfMembers, TID) values (416, 4, 615);
-insert into teams (leaderId, noOfMembers, TID) values (693, 2, 264);
-insert into teams (leaderId, noOfMembers, TID) values (614, 3, 859);
-insert into teams (leaderId, noOfMembers, TID) values (694, 2, 494);
-insert into teams (leaderId, noOfMembers, TID) values (266, 2, 907);
-insert into teams (leaderId, noOfMembers, TID) values (647, 3, 101);
-insert into teams (leaderId, noOfMembers, TID) values (173, 3, 736);
-insert into teams (leaderId, noOfMembers, TID) values (909, 2, 483);
-insert into teams (leaderId, noOfMembers, TID) values (594, 2, 446);
-
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (346, 4, 467);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (492, 3, 988);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (629, 3, 932);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (223, 3, 703);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (661, 4, 183);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (532, 2, 421);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (325, 4, 250);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (727, 4, 152);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (349, 4, 750);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (570, 2, 978);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (708, 3, 684);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (416, 4, 615);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (693, 2, 264);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (614, 3, 859);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (694, 2, 494);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (266, 2, 907);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (647, 3, 101);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (173, 3, 736);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (909, 2, 483);
+INSERT INTO teams (leaderId, noOfMembers, TID) VALUES (594, 2, 446);
