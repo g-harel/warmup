abf93868678ab2f47835148d1689224ac0969890
Philippe Beaudry <philippe.beaudry96@gmail.com>
2018-02-13 19:27:29 -0500

    Updated the GUI

---
+++ gui/index.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<!--Started with the basic Bootstrap 4 template-->
+<html lang="en">
+
+  <head>
+    <!-- Required meta tags always come first -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+
+    <style>
+    div {
+        width: 500px;
+        padding: 10px;
+        border: 5px solid gray;
+        margin: 0;
+    }
+
+    button {
+        background-color: #008CBA;  
+        border: none;
+        color: white;
+        padding: 15px 32px;
+        text-align: center;
+        text-decoration: none;
+        display: inline-block;
+        font-size: 16px;
+        margin: 4px 2px;
+        cursor: pointer;
+    }
+    </style>
+  </head>
+
+  <body>
+    <form action = "queries.php" method="post">
+
+        <h1>Query without inputs  <button type ="submit">Query</button></h1>
+
+        <p>Choose one of the following query (No input needed) and click query. (Refresh to remove Radio click under)</p>
+
+        <select name="table">
+          <option value=-1>Other queries</option>
+          <option value=0>1. Which student(s) is not a member of any team?</option>
+          <option value=1>2. For each team, list its members.</option>
+          <option value=2>3. Who was not present in the demo of a team?</option>
+          <option value=3>4. List the teams which that have <4 members.</option>
+          <option value=4>7. For each team that is not complete (<4 members), list the TID and the capacity to increase. (That is, if a team has 1 member, the capacity to increase would be 3).</option>
+        </select>
+
+      <br/><br/>
+
+      <h1>Query with inputs <button type ="submit">Query</button></h1>
+
+      <p>Choose one of the following query (input needed) and click query to run it after text input.</p>
+
+      <div>
+        <input type="radio" name="givenQ" value=0>5. Given a TID, list the names of the members.<br>
+        <label>Team id (Ex: 200):</label><input type="text" name ="tid"/>
+      </div>
+
+      <div>
+        <input type="radio" name="givenQ" value=1>6. Given a date, list all the teams that have demos on that day.<br>
+        <label>Date (Ex: 2018-01-17):</label><input type="text" name ="date"/>
+      </div>
+
+      <div>
+        <input type="radio" name="givenQ" value=2>8. Given a student Name or ID, find his/her team ID.<br>
+        <label>Student name(Ex: Pooh de Verson):</label><input type="text" name ="name8"/><br/><label>or student id Ex: 100):</label><input type ="text" name ="sid8"/>
+      </div>
+
+      <div>
+        <input type="radio" name="givenQ" value=3>9. Given a student Name or ID, find the names and SID of his/her teammates.<br>
+        <label>Student name(Ex: Pooh de Verson):</label><input type="text" name ="name9"/><br/><label>or student id (Ex: 100):</label><input type ="text" name ="sid9"/>
+      </div>
+    </form>
+  </body>
+</html>
---
+++ gui/queries.php
@@ -0,0 +1,167 @@
+<?php
+
+
+$servername = "lzc353.encs.concordia.ca";
+$password = "Logmein2";
+$username = "lzc353_4";
+$db = "lzc353_4";
+
+$conn = mysqli_connect($servername, $username, $password, $db);
+
+
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+//set the variables given by the user
+
+$tid = "";
+$date = "";
+$name8 = "";
+$sid8 = "";
+$name9 = "";
+$sid9 = "";
+
+if(isset($_POST["tid"])){
+  $tid = $_POST["tid"];
+  
+}
+
+if(isset($_POST["date"])){
+  $date = $_POST["date"];
+}
+
+if(isset($_POST["name8"])){
+  $name8 = $_POST["name8"];
+}
+
+if(isset($_POST["sid8"])){
+  $sid8 = $_POST["sid8"];
+}
+
+if(isset($_POST["name9"])){
+  $name9 = $_POST["name9"];
+}
+
+if(isset($_POST["sid9"])){
+  $sid9 = $_POST["name9"];
+}
+
+
+//list the queries here and we'll just call them when we have to
+$queriesFromDropdown = [
+  
+  //q1
+  "SELECT sid, name 
+  FROM students 
+  WHERE  sid IN (SELECT sid 
+                  FROM members 
+                  WHERE  members.tid NOT IN (SELECT tid 
+                                            FROM   teams));",
+  
+  //q2
+  "SELECT teams.tid, Group_concat(students.name SEPARATOR ', ')
+  FROM   members
+   JOIN students
+     ON students.sid = members.sid
+   JOIN teams
+     ON members.tid = teams.tid
+  GROUP BY teams.tid;",
+  
+  //q3
+  "SELECT sid, name
+    FROM   students
+    WHERE  sid NOT IN (SELECT sid
+    FROM   demos);",
+
+  //q4
+  "SELECT tid, leaderId
+    FROM   teams
+    WHERE noOfMembers < 4;",
+  
+  //q7
+  "SELECT tid, 4 - noOfMembers as capacity
+    FROM   teams
+    WHERE  noOfMembers < 4;",
+
+];
+
+$queriesWithInput = [
+  
+  //q5
+  "SELECT students.name
+    FROM   members
+    JOIN students
+    ON students.sid = members.sid
+    WHERE  members.tid =" . $tid . ";",
+    
+  //q6
+  "SELECT *
+    FROM   teams
+    JOIN demos
+    ON teams.leaderId = demos.sid
+    WHERE  demos.date = " . $date . ";", //2018-01-11
+
+  //q8
+  "SELECT members.tid
+    FROM students
+    JOIN members ON students.sid = members.sid
+    WHERE students.name = '" . $name8 . "' OR students.sid = '" . $sid8 . "';",
+
+  //9
+  "SELECT sid
+    FROM members
+    WHERE tid = (SELECT members.tid
+      FROM students
+      JOIN members ON students.sid = members.sid
+      WHERE students.name = '" . $name9 . "' OR students.sid = '" . $sid9 . "');",
+
+];
+
+
+
+if(isset($_POST["table"]) && $_POST["table"] != -1){
+  $sql = $queriesFromDropdown[$_POST["table"]];
+}
+
+if(isset($_POST['givenQ'])) {
+  $sql = $queriesWithInput[$_POST["givenQ"]];
+}
+
+echo "<h3>" .$sql. "</h3>";
+
+ $result = mysqli_query($conn, $sql);
+  
+
+ if (mysqli_num_rows($result) > 0) {
+
+    $arr = mysqli_fetch_assoc($result);
+    $arrKeys = array_keys($arr);
+
+    // output data of each row
+    echo "<table>";
+
+    for($i = 0; $i < sizeof($arrKeys); $i++){
+      echo "<th style='border: 1px solid #ddd; padding: 8px'>".$arrKeys[$i]."</th>";
+    }
+
+    $result = mysqli_query($conn, $sql);
+
+    while ($row = mysqli_fetch_row($result)) {
+         
+      echo '<tr>';
+          
+      for($r = 0; $r < sizeof($row); $r++){
+        echo "<td style='border: 1px solid #ddd; padding: 8px'>".$row[$r]."</td>";
+      }
+        
+      echo '</tr>';
+   }
+   echo "</table>";
+ }
+
+ else{
+   echo "<h1>No results</h1>";
+ }
+
+?>
